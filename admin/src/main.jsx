8c1601c2d27f48856de0989e89823bf9489544a8
Akash Mishra <akashmishra0389@gmail.com>
2025-08-06 12:01:39 +0530

    Rename AdminContext.jsx to adminContext.jsx to fix case issue

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import DoctorContextProvider from './context/DoctorContext.jsx'
 import AppContextProvider from './context/AppContext.jsx'
+//import AdminContextProvider from './context/adminContext.jsx'
 import AdminContextProvider from './context/adminContext.jsx'
 
 createRoot(document.getElementById('root')).render(
